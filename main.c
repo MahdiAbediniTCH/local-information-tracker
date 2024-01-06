f776e8c9c7ff4aeafecf2947359cff61d3c80701
Mahdi Abedini <m.abedini30@yahoo.com>
2024-01-06 23:52:06 +0330

    Added dictionary-like structure for command handling in main.c

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
+// Include required headers
+#include "utility.h"
 #include <stdio.h>
 
-int main(int argc, char *argv[]) // Main command handler function
+// Definitions and constants
+#define N_COMMANDS 0 // Number of allowed commands
+const char* COMMANDS[N_COMMANDS] = {""}; // Array of command strings
+int (*CMD_FUNCTIONS[N_COMMANDS])(int, char*) = {}; // Array of function pointers, indexes are ... to the indexes of strings in COMMANDS
+
+// Main command handler function
+int main(int argc, char *argv[])
 {
-    
+    if (argc == 1) {
+        // usage message
+        return 1;
+    }
+    int command_ind = searchStr(COMMANDS, argv[1], N_COMMANDS);
+    if (command_ind == -1) {
+        // command not found error
+        return 1;
+    }
+    int call_result;
+    call_result = (*CMD_FUNCTIONS[command_ind])(argc, argv);
 }
--- utility.h
+++ utility.h
@@ -1,6 +1,6 @@
 #include <string.h>
 
-int searchStr(char** arr, char* str, int last_ind)
+int searchStr(const char** arr, const char* str, int last_ind)
 {
     for (int i = 0; i <= last_ind; i++)
         if (strcmp(arr[i], str) == 0) return i;
