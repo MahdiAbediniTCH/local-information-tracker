ac1950650a36dec4e697f8618a38164151300c1c
Mahdi Abedini <m.abedini30@yahoo.com>
2024-01-08 18:58:42 +0330

    Started writing config command and added commandhandling.h

---
+++ commandhandling.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int cmdConfig(int argc, char *argv[])
+{
+    printf("yep, got it\n");
+}
--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 // Include required headers
-#include "utility.h"
 #include <stdio.h>
 
+#include "utility.h"
+#include "commandhandling.h"
+
 // Definitions and constants
-#define N_COMMANDS 0 // Number of allowed commands
-const char* COMMANDS[N_COMMANDS] = {""}; // Array of command strings
-int (*CMD_FUNCTIONS[N_COMMANDS])(int, char*) = {}; // Array of function pointers, indexes are ... to the indexes of strings in COMMANDS
+#define N_COMMANDS 2 // Number of allowed commands
+const char* COMMANDS[N_COMMANDS] = { // Array of command strings
+"config", 
+"init", 
+};
+
+int (*CMD_FUNCTIONS[N_COMMANDS])(int, char*[]) = {
+cmdConfig,
+
+}; // Array of function pointers, indexes are ... to the indexes of strings in COMMANDS
 
 // Main command handler function
 int main(int argc, char *argv[])
@@ -21,4 +30,5 @@ int main(int argc, char *argv[])
     }
     int call_result;
     call_result = (*CMD_FUNCTIONS[command_ind])(argc, argv);
+    
 }
